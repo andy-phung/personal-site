928bdc88ea65650dfcde05eac991d57b0f0f874d
Andy <andyphung64@gmail.com>
2023-02-24 18:14:48 -0800

    add home pg intro

--- src/about.js
+++ src/about.js
@@ -7,7 +7,7 @@ export class About extends React.Component {
             <div className='about_container'>
               <div className='about_left'>
               <p className='about_heading'><b>About me</b></p>
-                <p className='about_content'>Hey there! I'm Andy, a graduating senior at <a className='link' href="https://independence.esuhsd.org/">Independence High School</a> and an incoming freshman at MIT. </p>
+                <p className='about_content'>Hey there! I'm Andy, a graduating senior at <a className='link' href="https://independence.esuhsd.org/">Independence High School</a> and an incoming freshman at <a className='link' href="https://bcs.mit.edu/academic-program/course-6-9-computation-and-cognition">MIT</a>. </p>
               
               <p className='about_heading'><b>Fun facts</b></p>
               <ul className='about_content'>
@@ -15,7 +15,7 @@ export class About extends React.Component {
                 <li>my first name is not, in fact, actually andrew</li>
               </ul>
               <p className='about_heading'><b>Favorites</b></p>
-                <p className='about_content'>Visual media (movie/films, anime/manga, k-dramas, etc):</p>
+                <p id='visual_media_favs' className='about_content'>Visual media (movie/films, anime/manga, k-dramas, etc):</p>
                 <ol className='about_content'>
                   <li>Blue Period</li>
                   <li>Everything Everywhere All At Once</li>
